a68e751c0b32e52e8f14d59119c1ddf99600a6cb
Pier Fiedorowicz <pierfied@email.arizona.edu>
2019-03-12 09:25:27 -0700

    Initial layout of project structure.

---
+++ src/lmapr/lmapr.c
@@ -0,0 +1,5 @@
+//
+// Created by pierfied on 3/12/19.
+//
+
+#include "lmapr.h"
---
+++ src/lmapr/lmapr.h
@@ -0,0 +1,8 @@
+//
+// Created by pierfied on 3/12/19.
+//
+
+#ifndef TEMP_LMAPR_H
+#define TEMP_LMAPR_H
+
+#endif //TEMP_LMAPR_H
