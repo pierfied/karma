cc822a8bbd185fb2745b7a18a8922ce13581a5a3
Pier Fiedorowicz <pierfied@email.arizona.edu>
2019-03-18 12:55:38 -0700

    Implemented convergence to shear matrix calculation and associated tests.

--- lmapr/tools/transformations.py
+++ lmapr/tools/transformations.py
@@ -19,6 +19,10 @@ def shear2conv(g1, g2, lmax=None):
     :rtype: array-like (float)
     """
 
+    # Convert all array-like input to ndarrays.
+    g1 = np.asarray(g1)
+    g2 = np.asarray(g2)
+
     # Check input sizes.
     if len(g1) != len(g2):
         raise Exception('First and second shear components are not the same length.')
@@ -63,6 +67,10 @@ def conv2shear(k, lmax=None):
     :return: Shear map components.
     :rtype: Tuple of array-like (float)
     """
+
+    # Convert all array-like input to ndarrays.
+    k = np.asarray(k)
+
     # Check input size.
     if k.ndim != 1:
         raise Exception('Input convergence map must be a 1D array.')
@@ -91,3 +99,57 @@ def conv2shear(k, lmax=None):
     _, g1, g2 = hp.alm2map([gtlm, gelm, gblm], nside, lmax=lmax, verbose=False)
 
     return g1, g2
+
+
+def conv2shear_mats(nside, indices=None, lmax=None):
+    """Create matrices to perform convergence to shear transformations.
+
+    Builds the matrices by individually computing each convergence pixel's contribution to all shear pixels.
+
+    :param nside: Healpix nside parameter.
+    :type nside: int
+    :param indices: Healpix pixel numbers to build matrices for. Default: All pixels in the sky.
+    :type indices: array-like (int)
+    :param lmax: Maximum l mode to include in transformation.
+    :type lmax: int
+    :return: Transformation matrices for convergence to shear map components.
+    :rtype: Tuple of array-like (float)
+    """
+
+    # Get the number of pixels for the full-sky map.
+    npix = hp.nside2npix(nside)
+
+    # If indices is not set assume full-sky.
+    if indices is None:
+        indices = np.arange(npix)
+
+    # Convert all array-like input to ndarrays.
+    indices = np.asarray(indices)
+
+    # Check input.
+    if indices.ndim != 1:
+        raise Exception('Indices must be a 1D array.')
+    if len(indices) != len(np.unique(indices)):
+        raise Exception('Indices are not unique.')
+
+    # Get the number of pixels of interest.
+    ninds = len(indices)
+
+    # Create the transformation matrices.
+    A1 = np.zeros((ninds, ninds))
+    A2 = np.zeros_like(A1)
+
+    # Build transformation matrices by calculating each convergence pixel's contribution to each shear pixel.
+    for i, ind in enumerate(tqdm(indices, desc='Computing k2g matrix')):
+        # Create a convergence map with only the current pixel set to 1.
+        k = np.zeros(npix)
+        k[ind] = 1
+
+        # Calculate shears.
+        g1, g2 = conv2shear(k, lmax)
+
+        # Set matrix value for this convergence pixels contribution to all shear pixels.
+        A1[:, i] = g1[indices]
+        A2[:, i] = g2[indices]
+
+    return A1, A2
--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -56,7 +56,7 @@ def test_g2k_mismatch():
     g1 = np.zeros(100)
     g2 = np.zeros(101)
 
-    with np.testing.assert_raises(Exception):
+    with np.testing.assert_raises_regex(Exception, 'same length'):
         transformations.shear2conv(g1, g2)
 
 
@@ -66,7 +66,7 @@ def test_g2k_g1_dim():
     g1 = np.zeros((100, 100))
     g2 = np.zeros(100)
 
-    with np.testing.assert_raises(Exception):
+    with np.testing.assert_raises_regex(Exception, '1D'):
         transformations.shear2conv(g1, g2)
 
 
@@ -76,14 +76,83 @@ def test_g2k_g2_dim():
     g1 = np.zeros(100)
     g2 = np.zeros((100, 100))
 
-    with np.testing.assert_raises(Exception):
+    with np.testing.assert_raises_regex(Exception, '1D'):
         transformations.shear2conv(g1, g2)
 
 
 def test_k2g_dim():
     """Test that conv2shear raises an error when k is not a 1D array."""
 
-    k = np.zeros((100,100))
+    k = np.zeros((100, 100))
 
-    with np.testing.assert_raises(Exception):
+    with np.testing.assert_raises_regex(Exception, '1D'):
         transformations.conv2shear(k)
+
+
+def test_k2g_mats_full():
+    """Test that conv2shear_mats produce equivalent output to conv2shear."""
+
+    nside = 16
+    npix = hp.nside2npix(nside)
+    lmax = 32
+
+    A1, A2 = transformations.conv2shear_mats(nside, lmax=lmax)
+
+    k = np.random.standard_normal(npix)
+
+    g1, g2 = transformations.conv2shear(k, lmax=lmax)
+
+    g1_lin = A1 @ k
+    g2_lin = A2 @ k
+
+    np.testing.assert_almost_equal(g1, g1_lin)
+    np.testing.assert_almost_equal(g2, g2_lin)
+
+
+def test_k2g_mats_subset():
+    """Test that conv2shear_mats produce equivalent output to conv2shear for a subset of indices."""
+
+    nside = 16
+    npix = hp.nside2npix(nside)
+    lmax = 32
+
+    ninds = 100
+    inds = np.random.choice(npix, ninds, replace=False)
+
+    A1, A2 = transformations.conv2shear_mats(nside, indices=inds, lmax=lmax)
+
+    np.testing.assert_almost_equal(A1.shape[0], ninds)
+    np.testing.assert_almost_equal(A1.shape[1], ninds)
+    np.testing.assert_almost_equal(A2.shape[0], ninds)
+    np.testing.assert_almost_equal(A2.shape[1], ninds)
+
+    k = np.zeros(npix)
+    k[inds] = np.random.standard_normal(ninds)
+
+    g1, g2 = transformations.conv2shear(k, lmax=lmax)
+
+    g1_lin = A1 @ k[inds]
+    g2_lin = A2 @ k[inds]
+
+    np.testing.assert_almost_equal(g1[inds], g1_lin)
+    np.testing.assert_almost_equal(g2[inds], g2_lin)
+
+def test_k2g_mats_ind_dim():
+    """Test that conv2shear_mats raises an exception when a 2D array is passed."""
+
+    nside = 16
+
+    inds = np.arange(100).reshape([10,-1])
+
+    with np.testing.assert_raises_regex(Exception, '1D'):
+        transformations.conv2shear_mats(nside, indices=inds)
+
+def test_k2g_mats_ind_unique():
+    """Test that conv2shear_mats raises an exception when duplicate indices are passed."""
+
+    nside = 16
+
+    inds = np.array([0,0])
+
+    with np.testing.assert_raises_regex(Exception, 'unique'):
+        transformations.conv2shear_mats(nside, indices=inds)
